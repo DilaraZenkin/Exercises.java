c4d2f40cc9dc89d3431d539cd26ddab051a9b4f7
dilarazenkin <dilaraznkn@gmail.com>
2021-09-18 15:30:39 -0400

    first Draft

--- module-1/05_Command_Line_Programs/exercise-student/src/main/java/com/techelevator/Fibonacci.java
+++ module-1/05_Command_Line_Programs/exercise-student/src/main/java/com/techelevator/Fibonacci.java
@@ -1,8 +1,33 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class Fibonacci {
 
 	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+
+		System.out.println("Fibonacci Numbers");
+		System.out.println("Enter the number here-->>");
+		String numberString = keyboard.nextLine();
+
+
+		int numberInt = Integer.parseInt(numberString);
+
+		int firstNumber = 0;
+		int secondNumber = 1;
+		int summary = 0;
+		for(int i=2; i<numberInt; i++) {
+			summary = summary + i;
+
+			System.out.println(firstNumber + "" + secondNumber + "");
+
+
+
+		}
+
+
+
 
 	}
 
