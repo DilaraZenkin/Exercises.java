b4affd685efe85b9b5d3372675461d3140cc3ae8
dilarazenkin <dilaraznkn@gmail.com>
2021-11-15 18:59:15 -0500

    first draft

--- module-3/06_Intro_to_JavaScript/lecture-student/script.js
+++ module-3/06_Intro_to_JavaScript/lecture-student/script.js
@@ -9,6 +9,7 @@
  * They don't have a return type and the naming convention is camel-case.
  */
 function variables() {
+
   // Declares a variable where the value cannot be changed
   // Declares a variable those value can be changed
   // Declares a variable that will always be an array
